3b4a33c90fbac23bbec5b38a2b339bdf9d128a3f
Anton Allo <qqqqton1@yandex.ru>
2023-10-07 22:34:28 +0300

    Add comics list

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import AppHeader from '../appHeader/AppHeader';
+import AppBanner from '../appBanner/AppBanner';
 import RandomChar from '../randomChar/RandomChar';
 import CharList from '../charList/CharList';
 import CharInfo from '../charInfo/CharInfo';
+import ComicsList from '../comicsList/ComicsList';
 import ErrorBoundary from '../errorBoundary/ErrorBoundary';
 
 import decoration from '../../resources/img/vision.png';
@@ -17,7 +19,7 @@ const App = () => {
     <div className="app">
       <AppHeader />
       <main>
-        <ErrorBoundary>
+        {/* <ErrorBoundary>
           <RandomChar />
         </ErrorBoundary>
         <div className="char__content">
@@ -27,8 +29,12 @@ const App = () => {
           <ErrorBoundary>
             <CharInfo charId={selectedChar} />
           </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" />
+          </div>
+        <img className="bg-decoration" src={decoration} alt="vision" /> */}
+        <ErrorBoundary>
+          <AppBanner />
+          <ComicsList />
+        </ErrorBoundary>
       </main>
     </div>
   );
